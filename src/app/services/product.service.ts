9acd03e6e112365e6651d45af085da81888aad64
gizem <gizem@DESKTOP-5MUKOCP>
2021-03-21 09:56:01 +0300

    13 mart commit 3

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -7,15 +7,23 @@ import { Product } from "../models/product";
   providedIn: 'root'
 })
 export class ProductService {
-  apiUrl = "https://localhost:44305/api/products/getall";
+  apiUrl = "https://localhost:44305/api/";
 
 
   constructor(private HttpClient:HttpClient) { }
 
  
   getProducts():Observable<ListResponseModel<Product>>{
-    return this.HttpClient.get<ListResponseModel<Product>>(this.apiUrl);
+    let newPath=this.apiUrl + "products/getall"
+    return this.HttpClient.get<ListResponseModel<Product>>(newPath);
       }
+
+
+      getProductsByCategory(categoryId:number):Observable<ListResponseModel<Product>>{
+        let newPath=this.apiUrl + "products/getbycategory?categoryId=" + categoryId
+
+        return this.HttpClient.get<ListResponseModel<Product>>(this.apiUrl);
+          }
 }
 
 
